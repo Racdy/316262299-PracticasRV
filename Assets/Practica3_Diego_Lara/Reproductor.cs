3caba5194a5288a896dca1e7d7c1de48944a450c
Racdy <racdy_99@hotmail.com>
2023-03-24 21:17:04 -0600

    Practica 03
    Se subieron los archivos para la entrega de la práctica 03.
    -Video
    -Carpeta de escenas de práctica 03

---
+++ Assets/Practica3_Diego_Lara/Reproductor.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Sprites;
+using UnityEngine.UI;
+
+public class Reproductor : MonoBehaviour
+{
+    private VideoPlayer player;
+    public Button button;
+    public Sprite startSprite;
+    public Sprite stopSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeStartStop(){
+        if(player.isPlaying == false){
+            player.Play();
+            button.image.sprite = stopSprite;
+        }else{
+            player.Pause();
+            button.image.sprite = startSprite;
+        }
+    }
+}
